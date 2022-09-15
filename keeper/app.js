8d8dc48c20e7f7e268b9f41d9b7bdf6271c71d26
imiebaka <triple07even@gmail.com>
2022-09-15 14:18:24 +0100

    delete added

--- keeper/app.js
+++ keeper/app.js
@@ -26,7 +26,7 @@ else {
         // Create delete button
         const btn = document.createElement("button")
         btn.innerText = "Delete"
-
+        btn.setAttribute("onclick", "deleteNote(this," + index +")")
         p.appendChild(btn)
         noteArea.appendChild(p)
     }
@@ -38,16 +38,18 @@ form.addEventListener("submit", function (e) {
 
     const p = document.createElement("p")
     p.innerText = textArea.value
-
+    note.push(textArea.value)
+    
     // Create delete button
     const btn = document.createElement("button")
     btn.innerText = "Delete"
-
+    
+    btn.setAttribute("onclick", "deleteNote(this,"+ (note.length - 1)+")")
     p.appendChild(btn)
     noteArea.appendChild(p)
-
+    
+    
     // Put inside the array
-    note.push(textArea.value)
 
     // Add to localstorage
     const data = JSON.stringify(note)
@@ -58,3 +60,12 @@ form.addEventListener("submit", function (e) {
 })
 
 
+
+function deleteNote(e, currentIndex) {
+    e.parentElement.remove()
+    note.splice(currentIndex, 1)
+    const data = JSON.stringify(note)
+    localStorage.setItem("notes", data)
+
+
+}
