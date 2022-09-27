5e340481b0d76661fd37d378693d0e1d2c4094a9
imiebaka <triple07even@gmail.com>
2022-09-27 11:32:19 +0100

    ni 20

--- webApi/index.js
+++ webApi/index.js
@@ -1,10 +1,30 @@
-// const img = document.querySelector("img")
+const img = document.querySelector("img")
 
-fetch("https://dog.ceo/api/breeds/image/random")
-// Return a promise from the URL
-.then(function(response){
-    return response.json()
-})
-.then(function(result){
-    console.log(result);
-})
+// fetch("https://dog.ceo/api/breeds/image/random")
+// // Return a promise from the URL
+// .then(function(response){
+//     return response.json()
+// })
+// .then(function(result){
+//     console.log(result);
+// })
+
+
+
+// async function getDogs () {
+
+// }
+
+const getDogs = async (url) => {
+    const response = await fetch(url)
+    const result = await response.json()
+    return result
+}
+
+setInterval(() => {
+    getDogs("https://dog.ceo/api/breeds/image/random")
+    .then(res => {
+        img.src = res.message
+        console.log(res);
+    })
+}, 5000)
