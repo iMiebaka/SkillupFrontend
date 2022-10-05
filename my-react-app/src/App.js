13d8ad2fdc5dac0835d0537b7ba786fbb69e9e11
imiebaka <triple07even@gmail.com>
2022-10-05 12:42:16 +0100

    check this out

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+console.log(useState(""));
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [showTemplate, setShowtemplate] = useState(false)
+
+  const changeState = (e) => {
+    e.preventDefault()
+    setName(name)
+    setAge(age)
+    setShowtemplate(true)
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{marginTop: "17rem"}}>STATES & PROPS</h1>
+      {showTemplate && <p>My name is {name}, and i am {age} years old.</p>}
+    
+      <form onSubmit={changeState}>
+        <input value={name} onChange={e => setName(e.target .value)} type="text" placeholder='Enter name' /><br />
+        <input value={age} onChange={e => setAge(e.target .value)} type="tel" placeholder='Enter age' /><br />
+        <div className="boy" id="girl"></div>
+      <button type="submit" onClick={changeState}>change state</button>
+      </form>
+      
     </div>
   );
 }
