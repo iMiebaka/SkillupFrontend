a00b949380168bd9dc07770a97558e4aa08fb42b
imiebaka <triple07even@gmail.com>
2022-10-17 13:49:56 +0100

    for the love of mondays

---
+++ props-react/backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.json({ messages: "Login Successfully" })
+})
+
+app.listen(2000, () => {
+    console.log("App is running");
+})
