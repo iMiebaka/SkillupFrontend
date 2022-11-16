77351d1657dde2bce266c6404df746e03010b1c3
imiebaka <triple07even@gmail.com>
2022-11-16 21:11:07 +0100

    image-changer

---
+++ image-changer/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState, useRef } from "react";
+import "./App.css";
+
+const defaultAvatar = () => "https://www.w3schools.com/howto/img_avatar.png";
+
+function App() {
+  const [images, setImages] = useState([defaultAvatar()]);
+  const addImg = useRef();
+
+  const addImages = () => {
+    addImg.current.click();
+  };
+
+  const handleChange = (e) => {
+    const fileUploaded = e.target.files[0];
+
+    var reader = new FileReader();
+    reader.onload = function () {
+      let currentImages = [];
+      if (images.length == 1) {
+        currentImages = [reader.result, defaultAvatar()];
+      } else {
+        const sortedImage = [...images];
+        currentImages = [
+          ...sortedImage.slice(0, sortedImage.length - 1),
+          reader.result,
+          defaultAvatar(),
+        ];
+      }
+      setImages(currentImages);
+    };
+    reader.readAsDataURL(fileUploaded);
+  };
+
+  return (
+    <div className="App">
+      <input
+        onChange={handleChange}
+        hidden
+        ref={addImg}
+        type="file"
+        name=""
+        id=""
+        accept="*/images"
+      />
+      {images.map((i, k) => (
+        <img
+          key={k}
+          onClick={k == images.length - 1 ? addImages : null}
+          src={i}
+          width={200}
+          height={200}
+          alt=""
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
